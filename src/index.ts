9deb04ee4d07abee32ceea292f0b2dead1b7c58a
Sharmaketan <ketan.sharma@iensure.in>
2024-04-15 13:04:42 +0530

    backend api integration done

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,35 @@
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
+import "dotenv/config"
 
+import Stripe from "stripe"
+import { HTTPException } from "hono/http-exception"
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
+
+stripe.checkout.sessions
+
+console.log(process.env.STRIPE_PUBLISHABLE_KEY)
 const app = new Hono()
 
-app.get("/", (c) => {
-  return c.text("Hello Hono!")
+app.get("/checkout", async (c) => {
+  try {
+    const session = await stripe.checkout.sessions.create({
+      payment_method_types: ["card"],
+      line_items: [
+        {
+          price: "price_1P5jkUSEKxgA58oAcCXbe5Ta",
+          quantity: 1,
+        },
+      ],
+      mode: "payment",
+      success_url: "http://localhost:3000/success",
+      cancel_url: "http://localhost:3000/cancel",
+    })
+    return c.json(session)
+  } catch (error: any) {
+    console.error(error)
+    throw new HTTPException(500, { message: error?.message })
+  }
 })
 
 app.post("/", (c) => {
