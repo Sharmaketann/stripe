3700c6f5d7a5ded2dcfc5ff1d08ecee2dcd40ee9
Sharmaketan <ketan.sharma@iensure.in>
2024-04-15 15:18:19 +0530

    added: Frontend added to checkout backend

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,58 @@
 import { serve } from "@hono/node-server"
 import { Hono } from "hono"
 import "dotenv/config"
+import { cors } from "hono/cors"
+import { logger } from "hono/logger"
 
 import Stripe from "stripe"
 import { HTTPException } from "hono/http-exception"
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY!)
 
-stripe.checkout.sessions
-
-console.log(process.env.STRIPE_PUBLISHABLE_KEY)
 const app = new Hono()
+app.use("/*", cors()) // optional, only required if frontend is on different domain/port
+app.use(logger())
 
-app.get("/checkout", async (c) => {
+app.get("/", (c) => {
+  const html = `
+  <!DOCTYPE html>
+  <html>
+    <head>
+      <title>Checkout</title>
+      <script src="https://js.stripe.com/v3/"></script>
+    </head>
+    <body>
+      <h1>Checkout</h1>
+      <button id="checkoutButton">Checkout</button>
+
+      <script>
+        const checkoutButton = document.getElementById('checkoutButton');
+        checkoutButton.addEventListener('click', async () => {
+          const response = await fetch('/checkout', {
+            method: 'POST',
+            headers: {
+              'Content-Type': 'application/json',
+            },
+          });
+          const { id } = await response.json();
+          const stripe = Stripe('${process.env.STRIPE_PUBLISHABLE_KEY}');
+          await stripe.redirectToCheckout({ sessionId: id });
+        });
+      </script>
+    </body>
+  </html>
+`
+  return c.html(html)
+})
+
+app.get("/success", (c) => {
+  return c.text("Success!")
+})
+
+app.get("/cancel", (c) => {
+  return c.text("Hello Hono!")
+})
+
+app.post("/checkout", async (c) => {
   try {
     const session = await stripe.checkout.sessions.create({
       payment_method_types: ["card"],
@@ -30,10 +71,6 @@ app.get("/checkout", async (c) => {
     console.error(error)
     throw new HTTPException(500, { message: error?.message })
   }
-})
-
-app.post("/", (c) => {
-  return c.text("My POST request")
 })
 
 const port = 3000
